b062ffba16c59f0017f630b97bf071facbb64bad
gauravxgrewal <gauravxgrewal@gmail.com>
2025-10-28 19:51:28 +0530

    again padding fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaHeart } from 'react-icons/fa';
 
 export default function Footer({ socialLinks }) {
   return (
-    <footer className="bg-[#eaeff5] lg:hidden  col-span-1 md:col-span-2 lg:col-span-5 -m-5 mt-5 p-5">
+    <footer className="bg-[#eaeff5] lg:hidden  col-span-1 md:col-span-2 lg:col-span-5 -m-5 mt-1 p-5">
       <div className="w-full pt-2 flex  items-center justify-center ">
         
         <div className="text-gray-700 font-semibold">
